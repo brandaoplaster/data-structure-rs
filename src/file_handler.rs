42ee25771bd25dce7b6b3b75d0423727ccd40073
brandaoplaster <brandaoplaster@gmail.com>
2024-11-26 20:54:52 -0300

    feat: Adding load_from_file function in file_handler

--- src/file_handler.rs
+++ src/file_handler.rs
@@ -27,3 +27,31 @@ pub fn save_to_file(filename: &str, persons: &[Person]) -> io::Result<()> {
 
   Ok(())
 }
+
+pub fn load_from_file(filename: &str) -> io::Result<Vec<Person>> {
+  let file = File::open(filename)?;
+  let reader = BufReader::new(file);
+
+  let mut persons = Vec::new();
+  for line in reader.lines() {
+    let line = line?;
+    let fields: Vec<&str> = line.split('|').collect();
+
+    if fields.len() == 9 {
+      let person = Person {
+        id: fields[0].parse().unwrap_or_default(),
+        name: fields[1].to_string(),
+        lastname: fields[2].to_string(),
+        age: fields[3].parse().unwrap_or_default(),
+        salary: fields[4].parse().unwrap_or_default(),
+        profession: fields[5].to_string(),
+        gender: Gender::from_string(fields[6]).unwrap_or(Gender::Other),
+        education_level: EducationLevel::from_string(fields[7])
+          .unwrap_or(EducationLevel::HighSchool),
+        marital_status: MaritalStatus::from_str(fields[8]).unwrap_or(MaritalStatus::Single),
+      };
+      persons.push(person);
+    }
+  }
+  Ok(persons)
+}
