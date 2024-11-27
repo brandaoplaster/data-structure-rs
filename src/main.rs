9cdcf4fbd3da7d9cefd63e291f223bb4e5fdfa96
brandaoplaster <brandaoplaster@gmail.com>
2024-11-26 22:21:11 -0300

    feat: Updated main with functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use data_generator::generate_fake_people;
+use file_handler::{load_from_file, save_to_file};
+use person::Person;
+
+mod data_generator;
+mod file_handler;
+mod person;
+
 fn main() {
-    println!("Hello, world!");
+  let fake_persons: Vec<Person> = generate_fake_people(10);
+
+  let filename = "fake_persons.txt";
+
+  match save_to_file(filename, &fake_persons) {
+    Ok(_) => println!("File saved successfully!"),
+    Err(e) => println!("Error saving file: {}", e),
+  }
+
+  let loaded_persons = load_from_file(filename);
+  println!("Dados carregados: {:?}", loaded_persons);
 }
